bc1cd8323cc42e9b66aa8479a01c23d937d92d1d
MD Iftekhar Hossain Tanveer <73121601+tanveeriftekhar@users.noreply.github.com>
2024-08-02 17:42:57 +0300

    String concatenation

--- Day 1/main.py
+++ Day 1/main.py
@@ -1,3 +1,8 @@
 print ("Day 1 - Python Print Function\n")
 print ("The function is declared like this: \n")
-print ("print('what to print')")
+print ("print('what to print')") 
+
+    # String Concatenation 
+
+print ("Iftekhar" " Hossain" " Tanveer")
+print ("Iftekhar" + " " + "Hossain"+ " " +  " Tanveer")

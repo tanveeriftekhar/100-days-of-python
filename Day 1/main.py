e5ffeb3b0ab627a5dee2327eb10a232f18b09643
MD Iftekhar Hossain Tanveer <73121601+tanveeriftekhar@users.noreply.github.com>
2024-08-02 18:06:06 +0300

    String Manipulation

--- Day 1/main.py
+++ Day 1/main.py
@@ -6,3 +6,9 @@ print ("print('what to print')")
 
 print ("Iftekhar" " Hossain" " Tanveer")
 print ("Iftekhar" + " " + "Hossain"+ " " +  " Tanveer")
+
+    # String Manipulation
+print ("Day 1 - String Manipulation")
+print ("String concatenation is done with the " "+" " sign")
+print ('e.g. print("Hello " + "world")')
+print ("New lines can be created with a backslash and n.")

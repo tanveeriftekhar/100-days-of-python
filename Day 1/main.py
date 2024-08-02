b9d7fb9f8bb77023f244d5a906cedc6103f994eb
MD Iftekhar Hossain Tanveer <73121601+tanveeriftekhar@users.noreply.github.com>
2024-08-02 18:30:35 +0300

    input function

--- Day 1/main.py
+++ Day 1/main.py
@@ -12,3 +12,9 @@ print ("Day 1 - String Manipulation")
 print ("String concatenation is done with the " "+" " sign")
 print ('e.g. print("Hello " + "world")')
 print ("New lines can be created with a backslash and n.")
+
+    # Input function 
+print ("What is your name? ") #Only prints. Cannot take input 
+input ("What is your name? ") # Takes input as an answer 
+print('Hello '+ input("What is your name? "))
+

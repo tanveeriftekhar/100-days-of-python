3fd7dad098c2d255d59b0a8ca0e2df81ca6fb5e2
MD Iftekhar Hossain Tanveer <73121601+tanveeriftekhar@users.noreply.github.com>
2024-08-14 13:20:27 +0300

    Problem solved

--- Day 2/main.py
+++ Day 2/main.py
@@ -1,3 +1,24 @@
-num_char = len(input("What is your name?"))
-new_num_char = str(num_char)
-print ("Your name has " + new_num_char + " characters")
+# num_char = len(input("What is your name?"))
+# new_num_char = str(num_char)
+# print ("Your name has " + new_num_char + " characters")
+
+# # to see the type 
+# a = float(123)
+# print(type(a))
+
+# a = str("I am Tanveer")
+# print(type(a))
+
+# b = bool(True)
+# print(type(b))
+
+
+# Write a program that adds the digits in a 2 digit number, for example if the input was 35, then the output would be 3 + 5 
+two_digit_number = input("type a two digit number")
+first_digit = two_digit_number[0]
+second_digit = two_digit_number[1]
+first_number = int(first_digit)
+second_number = int(second_digit)
+equation = str(first_number + second_number)
+print(equation)
+print (first_digit + "+" + second_digit + "=" + equation)
